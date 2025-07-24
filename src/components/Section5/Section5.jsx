bbc856828b81c50f3e3475520bcb4a3d34214e7f
NallaAnjani <anjanikumarinalla@gmail.com>
2025-07-24 23:17:46 +0530

    abc

---
+++ src/components/Section5/Section5.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Section5.css";
+
+const Section5 = () => {
+  return (
+    <div className="section5">
+      <div className="stat-box">
+        <div className="stat">
+          <h2>100+</h2>
+          <p>Bookings Completed</p>
+        </div>
+        <div className="stat">
+          <h2>150+</h2>
+          <p>Happy Customers</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section5;
